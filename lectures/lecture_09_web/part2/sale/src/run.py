1aa23fad95439c5d8c2db9d78c8579db336e3607
Kirill <vk1r1ll@yandex.ru>
2019-11-14 13:47:32 +0300

    save after lecture

--- lectures/lecture_09_web/part2/sale/src/run.py
+++ lectures/lecture_09_web/part2/sale/src/run.py
@@ -5,3 +5,28 @@
 # Вернуть json в котором указать username, age и sale, которая рассчитывается по формуле round(age / 7).
 # Кука при запросе на сервис auth должна быть вида
 # {'session': '<длинный id>', 'technoatom': '<длинный id2>'}, её можно получить из запроса к сервису.
+import requests
+from flask import Flask, abort, jsonify, request
+from flask_login import (
+    LoginManager, UserMixin, login_required,
+    current_user, login_user, logout_user
+)
+
+app = Flask(__name__)
+HOST = '0.0.0.0'
+
+@app.route('/', methods=['GET'])
+@login_required
+def get_sale():
+    cook = request.cookies
+    resp = requests.request(method="get", url="http://auth:5000/about_me", cookies=cook)
+    age = resp.json()['age']
+    return round(age / 7)
+        
+
+def main():
+    app.run(host=HOST, debug=True)
+
+
+if __name__ == '__main__':
+    main()
